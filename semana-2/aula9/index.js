c58eda6beb1fdbab6429a5445d77b1c00a9d4372
Gabriel Augusto de  Oliveira <deoliveiragabriel88@gmail.com>
2021-08-18 23:51:52 -0300

    fiz o exercicio 9

--- semana-2/aula9/index.js
+++ semana-2/aula9/index.js
@@ -57,14 +57,13 @@ while(quantidadeAtual < quantidadeTotal){
 // c) Por fim, imprima o array com os nomes dos bichinhos no console
 
 const pergunta1 = Number(prompt("Quantos bichinhos de estimação vc tem?"))
-    if (pergunta1 === 0){
-        console.log("Que pena! Você pode adotar um pet!")
-    }
-    else if (pergunta1 >=1){
-        pergunta2 = prompt("Digite o nome do seu pet:")
-        nomePet = pergunta2
-        console.log(nomePet)
-    }
+if (pergunta1 === 0){
+    console.log("Que pena! Você pode adotar um pet!")
+} else if (pergunta1 >=1){
+    pergunta2 = prompt("Digite o nome do seu pet:")
+    nomePet = pergunta2
+    console.log(nomePet)
+}
 
 
 // 2) Considere que você tenha acesso a um array  (chamado de 'array original') que é composto somente de números. Baseando-se nisso, crie uma função para cada um dos itens abaixo, realizando as operações pedidas:
@@ -74,18 +73,56 @@ const pergunta1 = Number(prompt("Quantos bichinhos de estimação vc tem?"))
 // d) Escreva um programa que crie um novo array contendo strings, da seguinte forma: "O elemento do índex i é: numero". Depois, imprima este novo array.
 // e) Escreva um programa que imprima no console o maior e o menor números contidos no array original
 
-numeros = (1,2,3,4,5)
-const arrayNumeros = (numeros)
-    console.log("Exercico 2) escrita de código:", arrayNumeros)  //de novo ta imprimindo somente o ultimo numero do array no console (5)
+
+const arrayOriginal = [1,2,3,4,5] 
+for (i=0; i<arrayOriginal.length; i++){
+  console.log("Exercico 2A) escrita de código:", arrayOriginal[i])     
+}
+
+for (i=0; i<arrayOriginal.length; i++){
+  console.log("Exercico 2B) escrita de código:", arrayOriginal[i]/10)     
+}
+let soPares = []  
+for (i=0; i<arrayOriginal.length; i++){
+  if (arrayOriginal[i] %2 === 0 ){
+      soPares.push(arrayOriginal[i])
+  }
+}
+
+console.log("Exercico 2C) escrita de código:",soPares)
+
+  //D)
 
 
+ const novoArray = ["a","b","c","d"]
+ for (i=0; i<novoArray.length; i++){
+  console.log(`2C): O elemento do índex ${i} é: ${novoArray[i]}`)     
+}
 
+let maiorValor = arrayOriginal[0] 
+for (i=0; i<arrayOriginal.length; i++){
+    if (arrayOriginal[i] > maiorValor){
+        maiorValor = arrayOriginal[i]
+    }
+}
+
+let menorValor = arrayOriginal[0]
+for (i=0; i<arrayOriginal.length; i++){
+    if (arrayOriginal[i] < menorValor){
+        menorValor = arrayOriginal[i]
+    }
+}
+
+console.log("2D) o maior é:", maiorValor, "o menor é:", menorValor)
+
+// let i = arrayNumeros.length[]
+
+//     console.log("Exercico 2)B escrita de código:", arrayNumeros)
 
 
 // DESAFIOS
 // 1)Neste exercício vocês vão implementar uma brincadeira muito simples: "Adivinhe o número que estou pensando". Ele deve ser jogado entre duas pessoas. 
 // Inicialmente, uma das pessoas insere qual o número em que ela pensou. A outra pessoa tem que ficar chutando até acertar em cheio o número. Esta é uma tarefa difícil, então quem escolheu o número fica dando umas dicas para a outra pessoa, indicando se o número que ela pensou é maior ou menor do que o chute em si. Veja, abaixo, um exemplo de partida:
-
 // Vamos jogar!
 // O número chutado foi: 3
 // Errrrrrrrou, é maior
@@ -96,6 +133,55 @@ const arrayNumeros = (numeros)
 // O número chutado foi: 11
 // Acertou!!
 // O número de tentativas foi: 4 
+
+// modo A)
+// const numeroEscolhidoj1 = Number(prompt("Jocador 1: Escolha um numero!"))
+//   console.log("Vamos jogar!")
+//   let numeroEscolhidoj2 = -999
+//   let tentativas = 0
+//   for (;numeroEscolhidoj2 !== numeroEscolhidoj1;){
+//     tentativas += 1
+//     numeroEscolhidoj2 = Number(prompt("Jocador 2: Tente adivinha o numero escolhido!"))
+//       if (numeroEscolhidoj2 < numeroEscolhidoj1){
+//           console.log(`O número chutado foi: ${numeroEscolhidoj2}, Errou! O número escolhido é maior`)
+//         } 
+//       else if (numeroEscolhidoj2 > numeroEscolhidoj1){
+//           console.log(`O número chutado foi: ${numeroEscolhidoj2}, Errou! O número escolhido é menor`)
+//         }  
+//       else if (numeroEscolhidoj2 === numeroEscolhidoj1){
+//           console.log(`Acertou!!! O número de tentativas foi: ${tentativas}`)
+//       }
+//       console.log(`O número de tentativas foi: ${tentativas}`)
+      
+//     }
+
+
+// modo B)
+
+const numeroEscolhidoj1 = Number(prompt("Jocador 1: Escolha um numero!"))
+console.log("Vamos jogar!")
+let acertou = false
+let tentativas = 0
+while (!acertou){
+    tentativas += 1
+    numeroEscolhidoj2 = Number(prompt("Jocador 2: Tente adivinha o numero escolhido!"))
+    if (numeroEscolhidoj2 < numeroEscolhidoj1){
+        console.log(`O número chutado foi: ${numeroEscolhidoj2}, Errou! O número escolhido é maior`)
+    } else if (numeroEscolhidoj2 > numeroEscolhidoj1){
+        console.log(`O número chutado foi: ${numeroEscolhidoj2}, Errou! O número escolhido é menor`)
+    } else if (numeroEscolhidoj2 === numeroEscolhidoj1){
+      acertou = true  
+      console.log(`Acertou!!! O número de tentativas foi: ${tentativas}`)
+    }
+    console.log(`O número de tentativas foi: ${tentativas}`)
+    
+}
+
+
+
+
+
+
 
 
 
